c1c712cd8e260d6f89a5cf2710bed54c34ffbc7c
liyuetong <lyt1040755930@gmail.com>
2020-05-09 01:58:54 +0800

    add some keywords and it can reply

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
-if __name__ == '__main__':
-    print('hello')
+from telegram.ext import Updater, CommandHandler
+import config
+
+
+# Test code
+def hello(update, context):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+def nb(update, context):
+    update.message.reply_text("睿哥牛逼")
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+updater = Updater(config.token, use_context=True)
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('nb', nb))
+updater.dispatcher.add_handler(CommandHandler('start', start))
+
+updater.start_polling()
+updater.idle()
