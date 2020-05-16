dd67e8e23cfebacff9a2150d5d442f5d69d5f345
liyuetong <lyt1040755930@gmail.com>
2020-05-16 14:28:41 +0800

    add some functions

--- test.py
+++ test.py
@@ -1,23 +1,57 @@
 from telegram.ext import Updater, CommandHandler
 import config
 
+# Examples
 
-# Test code
+# Reply a message, will reply (which will notify) the person who sends the command.
 def hello(update, context):
     update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+        'Hello {}'.format(update.message.from_user.first_name)
+    )
 
-def nb(update, context):
-    update.message.reply_text("睿哥牛逼")
-
+# Send a message, will NOT reply the person who sends the command.
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+# Handling arguments. Need to set 'pass_args = True' in CommandHandler()
+# TODO: Display assignments which due today
+def today(update, context):
+    arg = context.args[0]
+    print("Here " + arg)
+
+# Display all assignments
+def all(update, context):
+    print("all")
+
+
+# Add Something
+def add(update, context):
+    for arg in context.args:
+        print(arg)
+
+
 
 updater = Updater(config.token, use_context=True)
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
-updater.dispatcher.add_handler(CommandHandler('nb', nb))
 updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(
+    CommandHandler(
+        'today',
+        today,
+        pass_args = True
+    )
+)
+
+updater.dispatcher.add_handler(
+    CommandHandler(
+        'add',
+        add,
+        pass_args = True
+    )
+)
+
+
 
 updater.start_polling()
 updater.idle()
