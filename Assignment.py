b8fa4be985fa175e75bb704526797b881c912fd6
Jerrypoi <jerry.poi.sz@gmail.com>
2020-05-31 13:04:17 +0800

    Add and show assignments

--- Assignment.py
+++ Assignment.py
@@ -2,11 +2,11 @@ class Assignment:
     _id_index = 0
 
     def __init__(self, title, due_time, *args, **kwargs):
-        self.id = self._id_index
+        self.id = Assignment._id_index
         self.title = title
         self.due_time = due_time
 
-        self._id_index += 1
+        Assignment._id_index += 1
 
     def __str__(self):
-        return str(self.id) + " : " + self.title + " 截止时间： " + str(self.due_time)
+        return str(self.id) + "-" + self.title + " " + str(self.due_time)
